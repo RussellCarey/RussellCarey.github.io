bced9f05c0325ca50d43218e638d5d8adb4e79f0
RussellCarey <62222824+RussellCarey@users.noreply.github.com>
2020-08-17 13:48:05 +0900

    Add files via upload

--- js.js
+++ js.js
@@ -14,6 +14,9 @@ if (WURFL.form_factor === "Tablet") {
 window.addEventListener("click", (l) => {
   if (!l.target.classList.contains("colorboxes__colorbox")) {
     boxes.forEach((element) => {
+      if (element.children[1]) {
+        element.children[1].classList.remove("colorboxes__colorbox__show");
+      }
       element.style.height = "50%";
       element.style.width = "20%";
     });
@@ -22,16 +25,16 @@ window.addEventListener("click", (l) => {
 
 boxes.forEach((e) => {
   e.addEventListener("click", (i) => {
-    console.log("clicked box");
     largeBox(i.target);
 
     boxes.forEach((element) => {
       if (element != i.target) {
         element.style.height = "50%";
-        element.style.width = "10%";
+        element.style.width = "5%";
 
         if (device === 1 || device === 2) {
           element.children[0].style.opacity = "0";
+          element.children[0].style.display = "none";
         }
       }
     });
@@ -41,6 +44,7 @@ boxes.forEach((e) => {
 function largeBox(box) {
   box.style.height = "100vh";
   box.style.width = "90vw";
+  box.children[1].classList.add("colorboxes__colorbox__show");
 
   if (device === 1 || device === 2) {
     box.children[0].style.opacity = "1";
