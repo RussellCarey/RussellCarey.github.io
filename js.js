8bebc5056576581edd2888cb9c3548eb8879676f
RussellCarey <62222824+RussellCarey@users.noreply.github.com>
2020-08-21 17:20:26 +0900

    Add files via upload

--- js.js
+++ js.js
@@ -1,9 +1,12 @@
 //form_factor -- May return any of these values: Desktop, Tablet, Smartphone, Feature Phone, Smart-TV, Robot, Other non-Mobile, Other Mobile
 
 let device = 4;
-pg = document.getElementById("page");
-boxes = document.querySelectorAll(".colorboxes__colorbox");
-back = document.getElementById("back");
+const cross = document.getElementById("cross");
+const test = document.getElementById("test");
+const boxDiv = document.getElementById("boxdiv");
+const boxes = document.querySelectorAll(".colorboxes__colorbox");
+const text = document.querySelectorAll(".colorboxes__colorbox__text");
+const back = document.getElementById("back");
 
 if (WURFL.form_factor === "Tablet") {
   device = 1;
@@ -13,50 +16,88 @@ if (WURFL.form_factor === "Tablet") {
   device = 0;
 }
 
-window.addEventListener("click", (l) => {
-  if (!l.target.classList.contains("colorboxes__colorbox")) {
-    boxes.forEach((element) => {
-      element.style.height = "50%";
-      element.style.width = "20%";
-      element.style.padding = "1rem";
-    });
-  }
+// gsap.to("h2.title", {duration: 1, opacity: 0.3});
+cross.addEventListener("click", () => {
+  location.reload();
 });
 
-back.addEventListener("click", () => {
-  pg.classList.remove("showpage");
-});
+addOriginal();
+function addOriginal() {
+  boxes.forEach((e) => {
+    e.addEventListener("click", partOne);
+  });
+}
 
-boxes.forEach((e) => {
-  e.addEventListener("click", (i) => {
-    largeBox(i.target);
+function increaseBoxesHeight(box) {
+  gsap.to(box, {
+    width: "100vw",
+    height: "100vh",
+    onComplete: box.classList.add("bigBoxes"),
+  });
 
-    boxes.forEach((element) => {
-      if (element != i.target) {
-        element.style.height = "100%";
-        element.style.width = "0%";
-        element.style.padding = "0";
+  gsap.to(cross, { opacity: 1 });
+}
 
-        if (device === 1 || device === 2) {
-          element.children[0].style.opacity = "0";
-          element.children[0].style.display = "none";
-        }
-      }
+function scaleBoxes(e) {
+  e.style.zIndex = "999";
+  text.forEach((t) => {
+    gsap.to(t, { opacity: 0, duration: 0.05 });
+  });
+  gsap.to(e, { delay: 0.8, duration: 1.2, scaleX: 10 });
+  gsap.to(test, { delay: 1, x: 0 });
+  gsap.to(test, {
+    delay: 1.9,
+    backgroundColor: "white",
+    duration: 1,
+    onComplete: () => {
+      test.style.overflowY = "scroll";
+    },
+  });
+  cross.style.display = "none";
+  cross.style.opacity = "0";
+}
+
+function fadeInText() {
+  gsap.to(text[0], { opacity: 1, delay: 1.1 });
+  gsap.to(text[1], { opacity: 1, delay: 1.4 });
+  gsap.to(text[2], { opacity: 1, delay: 1.7 });
+  gsap.to(text[3], { opacity: 1, delay: 2.0 });
+  gsap.to(text[4], { opacity: 1, delay: 2.8 });
+}
+
+function partOne() {
+  boxes.forEach((x) => {
+    increaseBoxesHeight(x);
+    setTimeout(() => {
+      x.removeEventListener("click", partOne);
+      x.addEventListener("click", scaleBoxes.bind(null, x));
+      backButton(back);
+    }, 2000);
+  });
+  fadeInText();
+}
+
+function backButton(b) {
+  b.addEventListener("click", () => {
+    cross.style.display = "initial";
+    gsap.to(cross, { opacity: 1 });
+    test.style.overflow = "hidden";
+    test.style.backgroundColor = "transparent";
+    gsap.to(test, { x: "-200vw" });
+    boxes.forEach((e) => {
+      e.removeEventListener("click", scaleBoxes.bind);
+      returnBoxes(e);
     });
   });
-});
+}
 
-function largeBox(box) {
-  box.style.height = "100vh";
-  box.style.width = "100vw";
-
-  if (device === 1 || device === 2) {
-    if (box.children[0]) {
-      box.children[0].style.opacity = "1";
-    }
-  }
-
+function returnBoxes(e) {
+  e.classList.remove("bixBoxes");
+  gsap.to(e, { delay: 1, scale: 1, duration: 1 });
   setTimeout(() => {
-    pg.classList.add("showpage");
-  }, 800);
+    e.style.zIndex = "500";
+  }, 3000);
+  text.forEach((t) => {
+    gsap.to(t, { delay: 2, duration: 1.5, opacity: 1 });
+  });
 }
